1c8196e351d8207088a7e661992aaf302d5bb1b9
Space Nerd <florianpospiech77@gmail.com>
2023-07-20 22:22:15 +0200

    add open html file functon

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from bs4 import BeautifulSoup
 
 class WebScarper:
-    def __init__(self):
-        pass
+    def __init__(self, html_file):
+        self.soup = BeautifulSoup()
+        self.html_file = html_file
+        self.open_html()
+        self.run()
+
+    def open_html(self):
+        with open(self.html_file) as fp:
+            self.soup = BeautifulSoup(fp, "lxml")
+
+    def run(self):
+        print(self.soup.prettify())
+
 
 if __name__ == "__main__":
     print("running...")
-    WebScarper()
+
+    # Add your HTML File
+    WebScarper("index.html")
